6bc43acb3df81035a163f72d4e4f72c891f74e68
Lamer3011 <jssk383@gmail.com>
2022-09-16 14:14:05 -0400

    Add project files.

---
+++ laba2_3kurs/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿
+namespace laba2_3kurs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_P = new System.Windows.Forms.TextBox();
+            this.textBox_Key = new System.Windows.Forms.TextBox();
+            this.textBox_C = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.xor_but = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox_C_ = new System.Windows.Forms.TextBox();
+            this.textBox_Key_ = new System.Windows.Forms.TextBox();
+            this.textBox_P_ = new System.Windows.Forms.TextBox();
+            this.StrP16 = new System.Windows.Forms.TextBox();
+            this.StrKey16 = new System.Windows.Forms.TextBox();
+            this.StrC16 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_P
+            // 
+            this.textBox_P.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_P.Location = new System.Drawing.Point(56, 36);
+            this.textBox_P.Name = "textBox_P";
+            this.textBox_P.Size = new System.Drawing.Size(257, 29);
+            this.textBox_P.TabIndex = 0;
+            // 
+            // textBox_Key
+            // 
+            this.textBox_Key.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_Key.Location = new System.Drawing.Point(81, 82);
+            this.textBox_Key.Name = "textBox_Key";
+            this.textBox_Key.Size = new System.Drawing.Size(232, 29);
+            this.textBox_Key.TabIndex = 1;
+            // 
+            // textBox_C
+            // 
+            this.textBox_C.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_C.Location = new System.Drawing.Point(57, 127);
+            this.textBox_C.Name = "textBox_C";
+            this.textBox_C.Size = new System.Drawing.Size(256, 29);
+            this.textBox_C.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "P =";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Key = ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "C =";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(113, 181);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 44);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // xor_but
+            // 
+            this.xor_but.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.xor_but.Location = new System.Drawing.Point(342, 73);
+            this.xor_but.Name = "xor_but";
+            this.xor_but.Size = new System.Drawing.Size(76, 43);
+            this.xor_but.TabIndex = 7;
+            this.xor_but.Text = "XOR";
+            this.xor_but.UseVisualStyleBackColor = true;
+            this.xor_but.Click += new System.EventHandler(this.xor_but_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(604, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 24);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "= C";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(604, 82);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 24);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "= Key";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(605, 127);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 24);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "= P";
+            // 
+            // textBox_C_
+            // 
+            this.textBox_C_.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_C_.Location = new System.Drawing.Point(436, 45);
+            this.textBox_C_.Name = "textBox_C_";
+            this.textBox_C_.Size = new System.Drawing.Size(163, 29);
+            this.textBox_C_.TabIndex = 11;
+            // 
+            // textBox_Key_
+            // 
+            this.textBox_Key_.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_Key_.Location = new System.Drawing.Point(436, 87);
+            this.textBox_Key_.Name = "textBox_Key_";
+            this.textBox_Key_.Size = new System.Drawing.Size(163, 29);
+            this.textBox_Key_.TabIndex = 12;
+            // 
+            // textBox_P_
+            // 
+            this.textBox_P_.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_P_.Location = new System.Drawing.Point(436, 127);
+            this.textBox_P_.Name = "textBox_P_";
+            this.textBox_P_.Size = new System.Drawing.Size(163, 29);
+            this.textBox_P_.TabIndex = 13;
+            // 
+            // StrP16
+            // 
+            this.StrP16.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StrP16.Location = new System.Drawing.Point(707, 127);
+            this.StrP16.Name = "StrP16";
+            this.StrP16.Size = new System.Drawing.Size(163, 29);
+            this.StrP16.TabIndex = 33;
+            // 
+            // StrKey16
+            // 
+            this.StrKey16.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StrKey16.Location = new System.Drawing.Point(707, 87);
+            this.StrKey16.Name = "StrKey16";
+            this.StrKey16.Size = new System.Drawing.Size(163, 29);
+            this.StrKey16.TabIndex = 32;
+            // 
+            // StrC16
+            // 
+            this.StrC16.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StrC16.Location = new System.Drawing.Point(707, 45);
+            this.StrC16.Name = "StrC16";
+            this.StrC16.Size = new System.Drawing.Size(163, 29);
+            this.StrC16.TabIndex = 31;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(876, 127);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 24);
+            this.label7.TabIndex = 30;
+            this.label7.Text = "= P";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(875, 82);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 24);
+            this.label8.TabIndex = 29;
+            this.label8.Text = "= Key";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(875, 45);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(39, 24);
+            this.label9.TabIndex = 28;
+            this.label9.Text = "= C";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(499, 9);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(20, 24);
+            this.label10.TabIndex = 34;
+            this.label10.Text = "2";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(766, 9);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(30, 24);
+            this.label11.TabIndex = 35;
+            this.label11.Text = "16";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(963, 251);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.StrP16);
+            this.Controls.Add(this.StrKey16);
+            this.Controls.Add(this.StrC16);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox_P_);
+            this.Controls.Add(this.textBox_Key_);
+            this.Controls.Add(this.textBox_C_);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.xor_but);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_C);
+            this.Controls.Add(this.textBox_Key);
+            this.Controls.Add(this.textBox_P);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_P;
+        private System.Windows.Forms.TextBox textBox_Key;
+        private System.Windows.Forms.TextBox textBox_C;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button xor_but;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox_C_;
+        private System.Windows.Forms.TextBox textBox_Key_;
+        private System.Windows.Forms.TextBox textBox_P_;
+        private System.Windows.Forms.TextBox StrP16;
+        private System.Windows.Forms.TextBox StrKey16;
+        private System.Windows.Forms.TextBox StrC16;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ laba2_3kurs/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba2_3kurs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void xor_but_Click(object sender, EventArgs e)
+        {
+            // 2 code
+            string strP = textBox_P.Text;
+            int intP = Convert.ToInt32(strP, 2);
+            string strKey = textBox_Key.Text;
+            int intKey = Convert.ToInt32(strKey, 2);
+            int intC = intP ^ intKey;
+            string strC = Convert.ToString(intC, 2);
+            //16 code
+            int P16 = Convert.ToInt32(strP, 16);
+            int Key16 = Convert.ToInt32(strKey, 16);
+            int C16 = Convert.ToInt32(strC, 16);
+
+            textBox_C_.Text = strC;
+            textBox_Key_.Text = strKey;
+            textBox_P_.Text = strP;
+            //
+            StrC16.Text = C16.ToString();
+            StrKey16.Text = Key16.ToString();
+            StrP16.Text = P16.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox_P.Text = "";
+            textBox_Key.Text = "";
+            textBox_C.Text = "";
+        }
+    }
+}
